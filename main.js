081f2e7d792ed39cd30e588045a12f65543c6223
BriceBasty <brice.basty1@gmail.com>
2018-09-05 14:25:48 +0200

    Cleanup

--- main.js
+++ main.js
@@ -1,15 +1,48 @@
+// Select container id, data to variable
 const container = document.querySelector('#container');
 
-for (let i = 0; i <= 15; i++) {
-	container.innerHTML += '<div class="gridElement">Test</div>';
+// Select button, data to variable
+const button = document.querySelector('button');
+
+// Select all gridElement class, data to variable
+const gridElement = Array.from(document.querySelectorAll('.gridElement'));
+
+// Loop to create divs
+for (let i = 0; i <= 15; i += 1) {
+  container.innerHTML += "<div class='gridElement'>Test</div>";
 }
 
-//Return all the grid elements into a variable
-const gridElement = document.querySelectorAll('.gridElement');
-
-//Add blackBackground to each gridElement
-Array.from(gridElement).forEach(gridElement => {
-    gridElement.addEventListener('mouseover', function(event) {
-		gridElement.classList.add('blackBackground');
+// Add blackBackground to each gridElement
+function addBlackBackground() {
+  gridElement.forEach((gridElement) => {
+    gridElement.addEventListener('mouseover', () => {
+      gridElement.classList.add('blackBackground');
     });
-});
+  });
+}
+
+function removeBlackBackground() {
+  gridElement.forEach((gridElement) => {
+    gridElement.classList.remove('blackBackground');
+  });
+}
+
+function askHowManySquares() {
+  const squaresNumber = window.prompt('How many squares per side for next grid?');
+  return squaresNumber;
+}
+
+function createGrid(squaresNumber) {
+  container.style.grid = `repeat(${squaresNumber}, 1fr) / repeat(${squaresNumber}, 1fr)`;
+}
+
+function clickButton() {
+  button.addEventListener('click', () => {
+    removeBlackBackground();
+    const squares = askHowManySquares();
+    createGrid(squares);
+  });
+}
+
+addBlackBackground();
+clickButton();
